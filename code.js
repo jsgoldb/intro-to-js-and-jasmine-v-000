0c4e58736ea11e81b077775dd8ceb84afa9e8534
jsgoldb <jgoldb@yahoo.com>
2016-04-18 17:02:39 -0500

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function favIceCream(name) {
+  return "I love " + name;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
+
+function theTruth() {
+  return true;
+}
+
+
+
